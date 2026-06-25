c512ea5e2804537420f9a509a5b1580eef685807
Cj-765 <cr372218@gmail.com>
2026-06-25 23:10:52 +0800

    Update HARD-D.py

--- HARD-D.py
+++ HARD-D.py
@@ -1,4 +1,5 @@
 import librosa as lb
+import librosa.util as lbu
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
@@ -17,11 +18,15 @@ filename = os.path.join("path", "to", "filename.wav")
 raw_audio, sr = lb.load(filename, sr = 22050, mono = True) 
 
 # Normalize amplitude (epsilon guards against silent recordings)
-norm_audio = raw_audio / (np.max(np.abs(raw_audio)) + 1e-9)
+def normalize_audio(x):
+    return x / (np.max(np.abs(x)) + 1e-9)
+
+norm_audio = normalize_audio(raw_audio)
 
 #pre-emphasis amplifier
-  
-emp_audio = np.append(norm_audio[:1], norm_audio[1:] - 0.97 * norm_audio[:-1])
+
+def pre_emp(x, coeff = 0.97):  
+    return np.append(x[:1], x[1:] - coeff * x[:-1])
 
 #filter function
 
@@ -34,4 +39,19 @@ def filter_audio (emp_audio, sr, lowcut, highcut, order = 4):
     return y
 #May adjust lowcut and highcut depending on the desired frequency(hz) level
 filt_audio = filter_audio(emp_audio, sr = 22050, lowcut = 1000, highcut = 8000 )
+
+#Audio amplification
+
+def apply_gain(x, gain_factor):
+    return  np.clip(x * gain_factor, -1.0, 1.0)
+
+filt_audio = normalize_audio(filt_audio)
+amp_audio = apply_gain(filt_audio, gain_factor = 2.0)
+peak_audio = amp_audio / np.max(np.abs(amp_audio))
+
+mu = 255
+mu_audio = np.sign(peak_audio) * np.log1p(mu * np.abs(peak_audio)) / np.log1p(mu)
+
+
+
 
